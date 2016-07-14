8cf87be9f35425b60a757211f9190dd5990f4404
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2016-07-14 16:39:39 +0100

    Change to compile stub library as extension

--- plugins/excalibur/setup.py
+++ plugins/excalibur/setup.py
@@ -5,12 +5,15 @@ import os
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-# Define the extension module
-fem_api_source_path='fem_api_source'
-fem_api_sources = ['fem_api_wrapper.c', 'femApi.cpp', 'ExcaliburFemClient.cpp', 'FemApiError.cpp']
+# Define the real and stub fem_api extension modules - note these are not true python extension 
+# modules in the sense that they cannot be used directly but need to be wrapped with ctypes, but
+# we use the setuptools Extension mechanism to manage them anyway
 
-fem_api = Extension('excalibur.fem_api', sources=[
-    os.path.join(fem_api_source_path, source) for source in fem_api_sources
+fem_api_stub_source_path='fem_api_stub'
+fem_api_stub_sources = ['femApi.cpp', 'ExcaliburFemClient.cpp', 'FemApiError.cpp']
+ 
+fem_api_stub = Extension('excalibur.fem_api_stub', sources=[
+    os.path.join(fem_api_stub_source_path, source) for source in fem_api_stub_sources
 ])
 
 setup(
@@ -20,7 +23,7 @@ setup(
     url='https://github.com/timcnicholls/odin',
     author='Tim Nicholls',
     author_email='tim.nicholls@stfc.ac.uk',
-    ext_modules=[fem_api],
+    ext_modules=[fem_api_stub],
     packages = find_packages(),
     install_requires=required,
 )
