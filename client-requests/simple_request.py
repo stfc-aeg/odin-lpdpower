c96b3a8f5e40d7ebbd9018d1bd1d904a12f1e621
Christian Angelsen <dw96@users.noreply.github.com>
2016-08-05 11:56:39 +0100

    Increased temp sensors to 11

--- client-requests/simple_request.py
+++ client-requests/simple_request.py
@@ -6,5 +6,5 @@ theLot = requests.get('http://beagle03.aeg.lan:8888/api/0.1/lpdpower/')
 
 temp0 = theLot.json()['temperature']['sensors']['0']['temperature']
 
-for index in range(9):
+for index in range(11):
     print "Temp{}: {:.1f}C".format(index, theLot.json()['temperature']['sensors'][str(index)]['temperature'])
