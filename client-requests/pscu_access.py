0ef14423a09d959a85a88a81cce8ee09c93eb9ca
Christian Angelsen <dw96@users.noreply.github.com>
2016-08-11 12:02:12 +0100

    pscu_access.py - Finished adding all sensors  - Code Testing time..

--- client-requests/pscu_access.py
+++ client-requests/pscu_access.py
@@ -62,14 +62,46 @@ class PSCUAccess(object):
 
     def getKey(self, path, aKey):
         rp = requests.get(path)
-##        print "Going to access path: \n\t'{}'".format(path)
-        # Check key value
-        rp = requests.get(path)
         if rp.status_code != 200:
             print "Error {}: getKey() failed on key '{}'".format(rp.status_code, aKey)
-##        else:
-##            print "Successfully read key: '{}' and has value: '{}' ".format(aKey, rp.json()[aKey])
         return rp.json()[aKey]
+
+    def setKey(self, path, aKey, aValue):
+        payload = {aKey: aValue}
+        # DEBUG:
+        rp = requests.get(path)
+        print "Before modification: (Code: {}) received: \n\t'{}'".format(rp.status_code, rp.text[:100])
+        rep = requests.put(path, data=json.dumps(payload), headers=self.headers)
+        #
+        if rep.status_code != 200:
+            print "error {}: Couldn't change key: '{}' to be '{}' in path: 'path'".format(aKey, aValue, path)
+        else:
+            print "Successfully changed key: '{}' to: '{}' ".format(aKey, aValue)
+
+    def refreshWebDict(self):
+        self.url = 'http://beagle03.aeg.lan:8888/api/0.1/lpdpower/'
+        self.response = requests.get(self.url)
+        self.dict     = self.response.json()
+
+    def testFanSpeeds(self):
+        # Manually set speed to 40%
+        print "Changing fan speed to 40%.."
+        fanPath = 'http://beagle03.aeg.lan:8888/api/0.1/lpdpower/fan'
+        speedPath = fanPath + '/currentspeed'
+        thePSCU.setKey(fanPath, 'target', 40)
+        for index in range(4):
+            print "\rCurrent Speed is:   ", thePSCU.getKey(speedPath, 'currentspeed'),
+            sys.stdout.flush()
+            time.sleep(1)
+
+        # Manually set speed to 80%
+        print "\nChanging fan speed to 80%.."
+        thePSCU.setKey(fanPath, 'target', 80)
+        for index in range(4):
+            print "\rCurrent Speed is:   ", thePSCU.getKey(speedPath, 'currentspeed'),
+            sys.stdout.flush()
+            time.sleep(1)
+        print ""
 
 
 if __name__ == "__main__":
@@ -81,7 +113,7 @@ if __name__ == "__main__":
 
     print "Overall Health: ", thePSCU.getKey(thePSCU.url, 'overall')
     print "Trace Status: {}, Latched: {}".format(thePSCU.getKey(thePSCU.url+'trace/overall', 'overall'),
-                                                thePSCU.getKey(thePSCU.url+'trace/overall', 'overall'))
+                                                thePSCU.getKey(thePSCU.url+'trace/latched', 'latched'))
     print "key enableall: ", thePSCU.getEnableall()
     print "key isarmed: ", thePSCU.getIsarmed()
 
@@ -102,6 +134,7 @@ if __name__ == "__main__":
 ##    dQuads = thePSCU.dict
 ##    quads = dQuads['quad']['quads']
 ##    channels = dQuads['quad']['quads']['0']['channels']
+##    print "Humidity:"
 ##    for quad in range(len( quads)):
 ##        print "QUAD NUMBER {}".format(quad)
 ##        for channel in range(len( channels)):
@@ -116,8 +149,7 @@ if __name__ == "__main__":
 ##            print "Ena? {}  Fdbk? {}  Fuse: {}  ".format(chEnable,chFeedback, chFuseVoltage),
 ##            print "Volt: {0:.3}V".format(chVoltage)
 ##        print "-----------"
-
-    print "________________________________________________________________"
+##    print "________________________________________________________________"
 
 ##    # Enable/Disabled a quad channel:
 ##    bEnable = False
@@ -127,25 +159,60 @@ if __name__ == "__main__":
 
     # Temperature:
     
-    temps = thePSCU.dict['temperature']['sensors']
-    channels = len(temps)
+    dTemperature = thePSCU.dict['temperature']
+    channels = len(dTemperature['sensors'])
+    print "Temperature:"
+    print "Status: {}  Latched: {}  ".format(dTemperature['overall'], dTemperature['latched'])
     for channel in range(channels):
-        temps[str(channel)]
-        chDisabled    = temps[str(channel)]['disabled']
-        chSetpoint    = temps[str(channel)]['setpoint']
-        chTemp        = temps[str(channel)]['temperature']
-        chTrace       = temps[str(channel)]['trace']
-        chTripped     = temps[str(channel)]['tripped']
-        print "Ch{}:  ".format(channel),
-        print "{0:.1f}C  ".format(chTemp),
-        print "Setpoint {0:.3}  ".format(chSetpoint),
-        print "Trace? {}  Enable? {}  ".format(chTrace, chDisabled),
-        print "Tripped: {} ".format(chTripped)
-    print "-----------"
+        chInfo        = dTemperature['sensors'][str(channel)]
+        print "Temp{0:>2}:  ".format(channel),
+        print "{0:>6.1f}C  Setpoint {1:>6.1f}C  ".format(chInfo['temperature'], chInfo['setpoint']),
+        print "Trace? {}  Enable? {:<1}  Tripped: {} ".format(chInfo['trace'], chInfo['disabled'],
+                                                           chInfo['tripped'])
+    print "________________________________________________________________"
 
     # Humidity
-##    dHumidity = thePSCU.getHumidity()
-##    dHumidity['sensors'].keys() # => [u'1', u'0']
-##    dHumidity['sensors']['0']   # => {u'disabled': 0, u'setpoint': 62.77198584890892, u'humidity': 46.02235371466141, u'trace': True, u'tripped': False}
+    
+    dHumidity = thePSCU.getHumidity()
+    #Above is equivalent to:
+    #thePSCU.getKey('http://beagle03.aeg.lan:8888/api/0.1/lpdpower/humidity', 'humidity')
+    
+    #dHumidity['sensors'].keys() # => [u'1', u'0']
+    #dHumidity['sensors']['0']   # => {u'disabled': 0, u'setpoint': 62.77198584890892, u'humidity': 46.02235371466141, u'trace': True, u'tripped': False}
+    print "Humidity:"
+    print "Status: {}  Latched: {}  ".format(dHumidity['overall'], dHumidity['latched'])
+    channels = len(dHumidity['sensors'])
+    for channel in range(channels):
+        chInfo        = dHumidity['sensors'][str(channel)]
+        print "Humidity{0:>2}:  ".format(channel),
+        print "{0:>6.1f}%  Setpoint {1:>6.1f}%  ".format(chInfo['humidity'], chInfo['setpoint']),
+        print "Trace? {}  Enable? {:<1}  Tripped: {} ".format(chInfo['trace'], chInfo['disabled'],
+                                                           chInfo['tripped'])
+    print "________________________________________________________________"
 
-    #print "\nkey fan: ", thePSCU.getFan()
+    # Pump
+    print "Pump info:"
+
+    chPump = thePSCU.getKey(thePSCU.url, 'pump')
+    print "Pump1 Status: {}  Latched: {}  ".format(chPump['overall'], chPump['latched'])
+    print "{0:>2.1f}l/min  Setpoint {1:>2.1f}l/min  ".format(chPump['flow'], chPump['setpoint']),
+    print "Tripped: {} ".format(chPump['tripped'])
+    
+    print "________________________________________________________________"
+    
+
+    
+    # Fan
+    print "Fan info:"
+
+    chFan = thePSCU.getFan()
+    print "Fan1 Status: {}  Latched: {}  ".format(chFan['overall'], chFan['latched'])
+    print "{0:>2.1f}Hz  Setpoint {1:>2.1f}Hz  ".format(chFan['currentspeed'], chFan['setpoint']),
+    print "{0:>2.1f}%  ".format(chFan['potentiometer']),
+    print "Tripped: {} ".format(chFan['tripped'])
+    
+    print "________________________________________________________________"
+
+    # Test that we can change the fan speed
+    #thePSCU.testFanSpeeds()
+    
