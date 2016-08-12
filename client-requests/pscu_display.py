4ae4c9f9cde511c02f6769afde1d46032b341067
Christian Angelsen <dw96@users.noreply.github.com>
2016-08-12 15:53:14 +0100

    Cleaned up code, display similar to webpage  - Address Enable/Disable visual discrepancy  (if  disabled=0, show 'Enable=True or 1')

--- client-requests/pscu_display.py
+++ client-requests/pscu_display.py
@@ -12,11 +12,11 @@ if __name__ == "__main__":
     print "________________________________________________________________"
     print "Replicating \"Overall Health\", i.e. left-hand side of the webpage"
 
-    print "Overall Health: ", thePSCU.getKey(thePSCU.url, 'overall')
-    print "Trace Status: {}, Latched: {}".format(thePSCU.getKey(thePSCU.url+'trace/overall', 'overall'),
+    print "Overall Health: {}".format(thePSCU.getOverall())
+    print "Trace Status:   {}, Latched: {}".format(thePSCU.getKey(thePSCU.url+'trace/overall', 'overall'),
                                                 thePSCU.getKey(thePSCU.url+'trace/latched', 'latched'))
-    print "key enableall: ", thePSCU.getEnableall()
-    print "key isarmed: ", thePSCU.getIsarmed()
+    print "key enableall:  {}".format(thePSCU.getEnableall())
+    print "key isarmed:    {}".format(thePSCU.getIsarmed())
 
     print "________________________________________________________________"
     
@@ -24,13 +24,13 @@ if __name__ == "__main__":
     # Developments..
 
     # Quads - Displays all key/value pairs, that needs wrapping in proper function call(s)
-    dQuads = thePSCU.dict
-    quads = dQuads['quad']['quads']
-    channels = dQuads['quad']['quads']['0']['channels']
+    quads = thePSCU.getKey(thePSCU.url + 'quad/quads', 'quads')
+    channels = quads['0']['channels']
     print "Quads:"
     for quad in range(len( quads)):
         print "------"
         print "Quad {}".format(quad)
+        
         for channel in range(len( channels)):
             quads[str(quad)]['channels'][str(channel)]
             chCurrent     = quads[str(quad)]['channels'][str(channel)]['current']
@@ -42,12 +42,13 @@ if __name__ == "__main__":
             print "{0:.3f}A  ".format(chCurrent),
             print "Ena? {}  Fdbk? {}  Fuse: {}  ".format(chEnable,chFeedback, chFuseVoltage),
             print "Volt: {0:.3}V".format(chVoltage)
+
     print "________________________________________________________________"
 
 
     # Temperature:
-    
-    dTemperature = thePSCU.dict['temperature']
+
+    dTemperature = thePSCU.getKey(thePSCU.url + 'temperature', 'temperature')
     channels = len(dTemperature['sensors'])
     print "Temperature:"
     print "Status: {}  Latched: {}  ".format(dTemperature['overall'], dTemperature['latched'])
@@ -61,12 +62,8 @@ if __name__ == "__main__":
 
     # Humidity
     
-    dHumidity = thePSCU.getHumidity()
-    #Above is equivalent to:
-    #thePSCU.getKey('http://beagle03.aeg.lan:8888/api/0.1/lpdpower/humidity', 'humidity')
-    
-    #dHumidity['sensors'].keys() # => [u'1', u'0']
-    #dHumidity['sensors']['0']   # => {u'disabled': 0, u'setpoint': 62.77198584890892, u'humidity': 46.02235371466141, u'trace': True, u'tripped': False}
+    dHumidity = thePSCU.getKey( thePSCU.url + 'humidity', 'humidity')
+
     print "Humidity:"
     print "Status: {}  Latched: {}  ".format(dHumidity['overall'], dHumidity['latched'])
     channels = len(dHumidity['sensors'])
@@ -88,12 +85,11 @@ if __name__ == "__main__":
     
     print "________________________________________________________________"
     
-
     
     # Fan
     print "Fan info:"
 
-    chFan = thePSCU.getFan()
+    chFan = thePSCU.getKey(thePSCU.url + 'fan', 'fan')
     print "Fan1 Status: {}  Latched: {}  ".format(chFan['overall'], chFan['latched'])
     print "{0:>2.1f}Hz  Setpoint {1:>2.1f}Hz  ".format(chFan['currentspeed'], chFan['setpoint']),
     print "Potentiometer: {0:>2.1f}%  ".format(chFan['potentiometer']),
