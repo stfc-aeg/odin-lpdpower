5eb5a685f603aecb1f4be13c8a4317e22a5d58cd
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2016-08-25 11:55:25 +0100

    Made LPDPowerAdapter handle exceptions cleanly.
    LPDPowerAdapter now handles exceptions from underlying PSCU data object cleanly,
    reporting and returning them with 400 status codes. Added setData and getData
    wrapper methods to PSCUData class to regularise underlying exceptions.
    
    Closes #2

--- plugins/lpdpower/lpdpower/adapter.py
+++ plugins/lpdpower/lpdpower/adapter.py
@@ -3,46 +3,63 @@ from tornado.escape import json_decode
 from concurrent import futures
 from tornado.ioloop import IOLoop
 from tornado.concurrent import run_on_executor
-from pscu_data import PSCUData
+from pscu_data import PSCUData, PSCUDataError
 import time
 import sys
+import logging
 
 
 class NullDevice():
+
     def write(self, s):
         pass
 
 
 class LPDPowerAdapter(ApiAdapter):
 
-	# Thread executor used for background tasks
-	executor = futures.ThreadPoolExecutor(max_workers=1)
-	
-	def __init__(self, **kwargs):
-		super(LPDPowerAdapter, self).__init__(**kwargs)
+        # Thread executor used for background tasks
+    executor = futures.ThreadPoolExecutor(max_workers=1)
 
-		sys.stdout = NullDevice() #Prevent I2C spam if devices aren't connected
+    def __init__(self, **kwargs):
+        super(LPDPowerAdapter, self).__init__(**kwargs)
 
-		self.pscuData = PSCUData()
+        sys.stdout = NullDevice()  # Prevent I2C spam if devices aren't connected
 
-		self.update_interval = self.options.get('update_interval', 0.05)
-		self.update_loop()
+        self.pscuData = PSCUData()
 
-	@request_types('application/json')
-	@response_types('application/json')
-	def get(self, path, request):
-		return ApiAdapterResponse(self.pscuData.dataTree.getData(path))	
+        self.update_interval = self.options.get('update_interval', 0.05)
+        self.update_loop()
 
-	@request_types('application/json')
-        @response_types('application/json')
-        def put(self, path, request):
-		data = json_decode(request.body)
-		self.pscuData.dataTree.setData(path, data)
-		return ApiAdapterResponse(self.pscuData.dataTree.getData(path))  
+    @request_types('application/json')
+    @response_types('application/json')
+    def get(self, path, request):
+        try:
+            response = self.pscuData.getData(path)
+            status_code = 200
+        except PSCUDataError as e:
+            response = {'error': str(e)}
+            status_code = 400
+        return ApiAdapterResponse(response, status_code=status_code)
 
-	#@run_on_executor
-	def update_loop(self):	
-		self.pscuData.pscu.updateLCD()
-		self.pscuData.pscu.pollAllSensors()
-		time.sleep(self.update_interval)
-		IOLoop.instance().add_callback(self.update_loop)
+    @request_types('application/json')
+    @response_types('application/json')
+    def put(self, path, request):
+        try:
+            data = json_decode(request.body)
+            self.pscuData.setData(path, data)
+            response = self.pscuData.getData(path)
+            status_code = 200
+        except PSCUDataError as e:
+            response = {'error': str(e)}
+            status_code = 400
+        except (TypeError, ValueError) as e:
+            response = {'error': 'Failed to decode PUT request body: {}'.format(str(e))}
+            status_code = 400
+        return ApiAdapterResponse(response, status_code=status_code)
+
+    #@run_on_executor
+    def update_loop(self):
+        self.pscuData.pscu.updateLCD()
+        self.pscuData.pscu.pollAllSensors()
+        time.sleep(self.update_interval)
+        IOLoop.instance().add_callback(self.update_loop)
--- plugins/lpdpower/lpdpower/pscu_data.py
+++ plugins/lpdpower/lpdpower/pscu_data.py
@@ -1,149 +1,173 @@
 from quad_data import QuadData
-from DataTree import DataTree
+from DataTree import DataTree, DataTreeError
 from pscu import PSCU
-from quad import Quad
-from tca9548 import TCA9548
 
-#Dummy class
-#class PSCU(object):
-#	def __init__(self):
-#		self.tca = TCA9548(0x70)
-#		quad = self.tca.attachDevice(5, Quad)
-#		self.quads = [quad] * 4
 
-#Data container for an individual temp sensor
 class TempData(object):
-	def __init__(self, pscu, number):
-		self.dataTree = DataTree({
-			"setpoint" : self.getSetPoint,
-			"temperature" : self.getTemp,
-			"trace" : self.getTrace,
-			"tripped" : self.getTripped,
-			"disabled" : self.getDisabled
-		})
+    """Data container for an individual temperature sensor."""
 
-		self.pscu = pscu
-		self.number = number
+    def __init__(self, pscu, number):
+        self.dataTree = DataTree({
+            "setpoint": self.getSetPoint,
+            "temperature": self.getTemp,
+            "trace": self.getTrace,
+            "tripped": self.getTripped,
+            "disabled": self.getDisabled
+        })
 
-	def getSetPoint(self):
-		return self.pscu.getTempSetPoint(self.number)
+        self.pscu = pscu
+        self.number = number
 
-	def getTemp(self):
-		return self.pscu.getTemperature(self.number)
+    def getSetPoint(self):
+        return self.pscu.getTempSetPoint(self.number)
 
-	def getTrace(self):
-		return self.pscu.getTempTrace(self.number)
+    def getTemp(self):
+        return self.pscu.getTemperature(self.number)
 
-	def getTripped(self):
-		return self.pscu.getTempTripped(self.number)
+    def getTrace(self):
+        return self.pscu.getTempTrace(self.number)
 
-	def getDisabled(self):
-		return self.pscu.getTempDisabled(self.number)
+    def getTripped(self):
+        return self.pscu.getTempTripped(self.number)
 
-#Data container for an individual humidity sensor
+    def getDisabled(self):
+        return self.pscu.getTempDisabled(self.number)
+
+
 class HumidityData(object):
-	def __init__(self, pscu, number):
-		self.dataTree = DataTree({
-			"humidity" : self.getHumidity,
-			"setpoint" : self.getSetPoint,
-			"tripped" : self.getTripped,
-			"trace" : self.getTrace,
-			"disabled" : self.getDisabled,
-		})
+    """Data container for an individual humidity sensor."""
 
-		self.pscu = pscu
-		self.number = number
+    def __init__(self, pscu, number):
+        self.dataTree = DataTree({
+            "humidity": self.getHumidity,
+            "setpoint": self.getSetPoint,
+            "tripped": self.getTripped,
+            "trace": self.getTrace,
+            "disabled": self.getDisabled,
+        })
 
-	def getHumidity(self):
-		return self.pscu.getHumidity(self.number)
+        self.pscu = pscu
+        self.number = number
 
-	def getSetPoint(self):
-                return self.pscu.getHSetPoint(self.number)
+    def getHumidity(self):
+        return self.pscu.getHumidity(self.number)
 
-	def getTripped(self):
-                return self.pscu.getHTripped(self.number)
+    def getSetPoint(self):
+        return self.pscu.getHSetPoint(self.number)
 
-	def getTrace(self):
-                return self.pscu.getHTrace(self.number)
+    def getTripped(self):
+        return self.pscu.getHTripped(self.number)
 
-	def getDisabled(self):
-		return self.pscu.getHDisabled(self.number)
+    def getTrace(self):
+        return self.pscu.getHTrace(self.number)
 
-#Data container for entire PSCU & Quads
+    def getDisabled(self):
+        return self.pscu.getHDisabled(self.number)
+
+
+class PSCUDataError(Exception):
+    """Simple exception container to wrap lower-level exceptions."""
+
+    pass
+
+
 class PSCUData(object):
-	def __init__(self, *args, **kwargs):
-		if len(args) and isinstance(args[0], PSCU):
-			self.pscu = args[0]
-		else:
-			self.pscu = PSCU(*args, **kwargs)
+    """Data container for entire PSCU & Quads."""
 
-		self.quadData = [QuadData(q) for q in self.pscu.quad]
-		self.tempData = [TempData(self.pscu, i) for i in range(11)]
-		self.humidityData = [HumidityData(self.pscu, i) for i in range(2)]
+    def __init__(self, *args, **kwargs):
+        if len(args) and isinstance(args[0], PSCU):
+            self.pscu = args[0]
+        else:
+            self.pscu = PSCU(*args, **kwargs)
 
-		self.dataTree = DataTree({
-			"quad" : {
-				"quads" : [q.dataTree for q in self.quadData],
-				"trace" : [self.traceQ0, self.traceQ1, self.traceQ2, self.traceQ3]
-			},
-			"temperature" : {
-				"sensors" : [t.dataTree for t in self.tempData],
-				"overall" : self.pscu.getTempOutput,
-				"latched" : self.pscu.getTempLatched,
-			},
-			"humidity" : {
-				"sensors" : [h.dataTree for h in self.humidityData],
-				"overall" : self.pscu.getHumidityOutput,
-				"latched" : self.pscu.getHumidityLatched,
-			},
-			"fan" : {
-				"target" : 0,
-				"currentspeed" : self.pscu.getFanSpeed,
-				"setpoint" : self.pscu.getFanSetPoint,
-				"potentiometer" : self.pscu.getFanPot,
-				"tripped" : self.pscu.getFanTripped,
-				"overall" : self.pscu.getFanOutput,
-				"latched" : self.pscu.getFanLatched,
-			},
-			"pump" : {
-				"flow" : self.pscu.getPumpFlow,	
-				"setpoint" : self.pscu.getPumpSetPoint,
-				"tripped" : self.pscu.getPumpTripped,
-				"overall" : self.pscu.getPumpOutput,
-				"latched" : self.pscu.getPumpLatched,
-			},
-			"trace" : {
-				 "overall" : self.pscu.getTraceOutput,
-				 "latched" : self.pscu.getTraceLatched,
+        self.quadData = [QuadData(q) for q in self.pscu.quad]
+        self.tempData = [TempData(self.pscu, i) for i in range(11)]
+        self.humidityData = [HumidityData(self.pscu, i) for i in range(2)]
+
+        self.dataTree = DataTree({
+            "quad": {
+                "quads": [q.dataTree for q in self.quadData],
+                "trace": [self.traceQ0, self.traceQ1, self.traceQ2, self.traceQ3]
+            },
+            "temperature": {
+                "sensors": [t.dataTree for t in self.tempData],
+                "overall": self.pscu.getTempOutput,
+                "latched": self.pscu.getTempLatched,
+            },
+            "humidity": {
+                "sensors": [h.dataTree for h in self.humidityData],
+                "overall": self.pscu.getHumidityOutput,
+                "latched": self.pscu.getHumidityLatched,
+            },
+            "fan": {
+                "target": 0,
+                "currentspeed": self.pscu.getFanSpeed,
+                "setpoint": self.pscu.getFanSetPoint,
+                "potentiometer": self.pscu.getFanPot,
+                "tripped": self.pscu.getFanTripped,
+                "overall": self.pscu.getFanOutput,
+                "latched": self.pscu.getFanLatched,
+            },
+            "pump": {
+                "flow": self.pscu.getPumpFlow,
+                "setpoint": self.pscu.getPumpSetPoint,
+                "tripped": self.pscu.getPumpTripped,
+                "overall": self.pscu.getPumpOutput,
+                "latched": self.pscu.getPumpLatched,
+            },
+            "trace": {
+                 "overall": self.pscu.getTraceOutput,
+                 "latched": self.pscu.getTraceLatched,
                         },
-			"overall": self.pscu.getHealth,
-			"arm" : True, #Output
-			"isarmed" : self.pscu.getArmed,
-			"enableall" : False #Output
-		})
+            "overall": self.pscu.getHealth,
+            "arm": True,  # Output
+            "isarmed": self.pscu.getArmed,
+            "enableall": False  # Output
+        })
 
-		self.dataTree.addCallback("enableall/", self.enableAll)
-		self.dataTree.addCallback("arm/", self.setArmed)
-		self.dataTree.addCallback("fan/target/", self.fanTarget)
+        self.dataTree.addCallback("enableall/", self.enableAll)
+        self.dataTree.addCallback("arm/", self.setArmed)
+        self.dataTree.addCallback("fan/target/", self.fanTarget)
 
-	def enableAll(self, path, value):
-		self.pscu.enableAll()
+    def getData(self, path):
+        """Get data from the underlying data tree.
 
-	def setArmed(self, path, value):
-		self.pscu.setArmed(value)
+        This method simply wraps underlying DataTree method so that an exceptions can be
+        re-raised with an appropriate PSCUDataError.
+        """
+        try:
+            return self.dataTree.getData(path)
+        except DataTreeError as e:
+            raise PSCUDataError(e)
 
-	def fanTarget(self, path, value):
-		self.pscu.setFanSpeed(value)
+    def setData(self, path, data):
+        """Set data in underlying data tree.
 
-	def traceQ0(self):
-		return self.pscu.getQuadTrace(0)
+        This method simply wraps underlying DataTree method so that an exceptions can be
+        re-raised with an appropriate PSCUDataError.
+        """
+        try:
+            self.dataTree.setData(path, data)
+        except DataTreeError as e:
+            raise PSCUDataError(e)
 
-	def traceQ1(self):
-                return self.pscu.getQuadTrace(1)
+    def enableAll(self, path, value):
+        self.pscu.enableAll()
 
-	def traceQ2(self):
-                return self.pscu.getQuadTrace(2)
+    def setArmed(self, path, value):
+        self.pscu.setArmed(value)
 
-	def traceQ3(self):
-                return self.pscu.getQuadTrace(3)
+    def fanTarget(self, path, value):
+        self.pscu.setFanSpeed(value)
 
+    def traceQ0(self):
+        return self.pscu.getQuadTrace(0)
+
+    def traceQ1(self):
+        return self.pscu.getQuadTrace(1)
+
+    def traceQ2(self):
+        return self.pscu.getQuadTrace(2)
+
+    def traceQ3(self):
+        return self.pscu.getQuadTrace(3)
