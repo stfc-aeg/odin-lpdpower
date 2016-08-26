7050372655afd7eed83084ebd41ea8f1ecc02109
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2016-08-26 11:20:49 +0100

    Cleanup formatting of quad.py, added NUM_CHANNELS definition

--- plugins/lpdpower/lpdpower/quad.py
+++ plugins/lpdpower/lpdpower/quad.py
@@ -7,84 +7,89 @@ from ad7998 import AD7998
 import time
 import logging
 
+
 class Quad(I2CContainer):
-	
-	def __init__(self):
-		I2CContainer.__init__(self)
-	
-		#Set up internal I2C devices
-		self.mcp = self.attachDevice(MCP23008, 0x20)
 
-		for i in range(4):
-			self.mcp.setup(i, MCP23008.OUT)
-			#self.mcp.pullup(i, True)
+    NUM_CHANNELS = 4
 
-		for i in range(4, 8):
-			self.mcp.setup(i, MCP23008.IN)
+    def __init__(self):
+        I2CContainer.__init__(self)
 
-		self.adcPower = self.attachDevice(AD7998, 0x22)
-		self.adcFuse = self.attachDevice(AD7998, 0x21)
+        # Set up internal I2C devices
+        self.mcp = self.attachDevice(MCP23008, 0x20)
 
-	def getChannelVoltage(self, channel):
-		if channel > 3 or channel < 0:
-                	raise I2CException("%s is not a channel on the Quad. Must be between 0 & 3" % channel)
+        for i in range(4):
+            self.mcp.setup(i, MCP23008.OUT)
+            # self.mcp.pullup(i, True)
 
-		return self.adcPower.readInput01(channel) * 5 * 16
+        for i in range(4, 8):
+            self.mcp.setup(i, MCP23008.IN)
 
-	def getChannelCurrent(self, channel):
-		if channel > 3 or channel < 0:
-                	raise I2CException("%s is not a channel on the Quad. Must be between 0 & 3" % channel)
+        self.adcPower = self.attachDevice(AD7998, 0x22)
+        self.adcFuse = self.attachDevice(AD7998, 0x21)
 
-                return self.adcPower.readInput01(channel + 4) * 5 * 4
+    def getChannelVoltage(self, channel):
+        if channel > 3 or channel < 0:
+            raise I2CException(
+                "%s is not a channel on the Quad. Must be between 0 & 3" % channel)
 
-	#Gets the voltage after the fuse should be more or less equal to 48V
-	def getFuseVoltage(self, channel):
-		if channel > 3 or channel < 0:
-			raise I2CException("%s is not a channel on the Quad. Must be between 0 & 3" % channel)
-		
-		return self.adcFuse.readInput01(channel) * 5 * 16
+        return self.adcPower.readInput01(channel) * 5 * 16
 
-	def getSupplyVoltage(self):
-		return self.adcFuse.readInput01(4) * 5 * 16
+    def getChannelCurrent(self, channel):
+        if channel > 3 or channel < 0:
+            raise I2CException(
+                "%s is not a channel on the Quad. Must be between 0 & 3" % channel)
 
-	#Sets an individual channel on or off
-	def setChannel(self, channel, enabled):
-		self.setChannels({channel : enabled})
+        return self.adcPower.readInput01(channel + 4) * 5 * 4
 
-	#Sets multiple channels on or off
-	def setChannels(self, channels):
-		data = {}
+    # Gets the voltage after the fuse should be more or less equal to 48V
+    def getFuseVoltage(self, channel):
+        if channel > 3 or channel < 0:
+            raise I2CException("%s is not a channel on the Quad. Must be between 0 & 3" % channel)
 
-		for channel in channels:
-			if channel > 3 or channel < 0:
-                        	raise I2CException("%s is not a channel on the Quad. Must be between 0 & 3" % channel)
+        return self.adcFuse.readInput01(channel) * 5 * 16
 
-			#If the channel is not currently in the desired state (on/off)
-			if self.isEnabled(channel) != channels[channel]:
-				data[channel] = True
+    def getSupplyVoltage(self):
+        return self.adcFuse.readInput01(4) * 5 * 16
 
+    # Sets an individual channel on or off
+    def setChannel(self, channel, enabled):
+        self.setChannels({channel: enabled})
 
-		#No channels to toggle
-                if not len(data):
-                        return
+    # Sets multiple channels on or off
+    def setChannels(self, channels):
+        data = {}
 
-		self.mcp.disableOutputs()
-                self.mcp.output_pins(data)
-                self.mcp.disableOutputs()
+        for channel in channels:
+            if channel > 3 or channel < 0:
+                raise I2CException(
+                    "%s is not a channel on the Quad. Must be between 0 & 3" % channel)
 
-	#Checks whether a channel is enabled
-	def isEnabled(self, i):
-		if i > 3 or i < 0:
-                        raise I2CException("%s is not a channel on the Quad. Must be between 0 & 3" % i)
+            # If the channel is not currently in the desired state (on/off)
+            if self.isEnabled(channel) != channels[channel]:
+                data[channel] = True
 
-		return self.mcp.input(i + 4) == 1
+        # No channels to toggle
+        if not len(data):
+            return
+
+        self.mcp.disableOutputs()
+        self.mcp.output_pins(data)
+        self.mcp.disableOutputs()
+
+    # Checks whether a channel is enabled
+    def isEnabled(self, i):
+        if i > 3 or i < 0:
+            raise I2CException("%s is not a channel on the Quad. Must be between 0 & 3" % i)
+
+        return self.mcp.input(i + 4) == 1
 
 if __name__ == "__main__":
-	tca = TCA9548(0x70)
-	quad = tca.attachDevice(0, Quad)
+    tca = TCA9548(0x70)
+    quad = tca.attachDevice(0, Quad)
 
-	quad.setChannels({0: True, 1: True, 2: True, 3:True})
-	for chan in range(4):
-		enabled = quad.isEnabled(chan)
+    quad.setChannels({0: True, 1: True, 2: True, 3: True})
+    for chan in range(4):
+        enabled = quad.isEnabled(chan)
 
-	#quad.printTest()
+    # quad.printTest()
