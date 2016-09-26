55cfeec471073e432a77f2659807df924ca41f11
Christian Angelsen <dw96@users.noreply.github.com>
2016-09-26 15:53:47 +0100

    Updated updateQuadSupply.py to use PSCUClient

--- client-requests/updateQuadSupply.py
+++ client-requests/updateQuadSupply.py
@@ -2,30 +2,8 @@
 
 ''' Demonstrate how temperature sensor data type is either int/bool for dictionary key 'disabled' '''
 
-from pscu_access import PSCUAccess
-thePSCU = PSCUAccess()
-
-# Within quad, what does the 'supply' key within each do?
-# I.e.:
-##>>> pp.pprint(d['quad']['quads'])
-##{ u'0': { u'channels': { u'0': { u'current': 0.11721611721611722,
-##                                 u'enable': False,
-##                                 u'feedback': False,
-##                                 u'fusevoltage': 0,
-##                                 u'voltage': 1.1135531135531136},
-##                         u'1': { u'current': 0.0,
-##                                 ...,
-##          u'supply': 0},                                    <--  This key !
-##  u'1': { u'channels': { u'0': { u'current': 20.0,
-##                                 ...,
-##          u'supply': 0},                                    <--  etc..
-##  u'2': { u'channels': { u'0': { u'current': 20.0,
-
-# NOTE: Like Quad's 'enable', if it's changed once then the int type value, becomes a key,value pair
-# i.e.
-#        u'supply': 0,
-# Becomes
-#        u'supply': {u'supply': 1}},
+from pscu_client import PSCUClient
+thePSCU = PSCUClient()
 
 def toggleQuadSupply(quad):
     
