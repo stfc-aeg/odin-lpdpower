002a93579922422adc370900896025fc4872b257
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2016-09-26 13:44:36 +0100

    Implemented generated test cases for all I2CDevice access methods

--- plugins/lpdpower/lpdpower/testing/test_i2c_device.py
+++ plugins/lpdpower/lpdpower/testing/test_i2c_device.py
@@ -8,45 +8,27 @@ import sys
 if sys.version_info[0] == 3:  # pragma: no cover
     from unittest.mock import Mock, call
 else:                         # pragma: no cover
-    from mock import Mock, call
+    from mock import Mock, MagicMock, call
 
 from nose.tools import *
 from functools import partial
 
-smbus_mock = Mock()
+smbus_mock = MagicMock()
 sys.modules['smbus'] = smbus_mock
 
 from lpdpower.i2c_device import I2CDevice, I2CException
 
-def setup_i2c_device_exception(method):
-    """
-    Decorator method for generated test cases to allow exceptions to be enabled and disabled
-    """
-    def wrap(func):
-        def wrapped_f(_self, exc_mode):
-
-            print 'in wrapper with exc_mode', exc_mode, 'func', func.__name__, 'method', method
-
-            if exc_mode == _self.EXC_MODE_NONE:
-                smbus_mock.side_effect = None
-                _self.device.disable_exceptions()
-            elif exc_mode == _self.EXC_MODE_TRAP:
-                smbus_mock.side_effect = IOError('boom')
-                _self.device.disable_exceptions()
-            elif exc_mode == _self.EXC_MODE_RAISE:
-                smbus_mock.side_effect = IOError('boom')
-                _self.device.enable_exceptions()
-            else:
-                raise Exception('Illegal exception test mode {}'.format(exc_mode))
-            func(_self)
-        return wrapped_f
-    return wrap
+class dummy_cm():
+    def __enter__(self):
+        return None
+    def __exit__(self, exc_type, exc_value, traceback):
+        return False
 
 class TestI2CDevice(object):
 
     EXC_MODE_NONE, EXC_MODE_TRAP, EXC_MODE_RAISE = range(3)
     EXC_MODES = [EXC_MODE_NONE, EXC_MODE_TRAP, EXC_MODE_RAISE]
-    EXC_MODE_NAME = ['exc_mode_node', 'exc_mode_trap', 'exc_mode_raise']
+    EXC_MODE_NAME = ['exception_mode_none', 'exception_mode_trap', 'exception_mode_raise']
 
     @classmethod
     def setup_class(cls):
@@ -77,32 +59,53 @@ class TestI2CDevice(object):
         self.device.disable_exceptions()
         assert_false(self.device._enable_exceptions)
 
-    def test_handle_error_no_exception(self):
-
-        smbus_mock_cached = smbus_mock.side_effect
-        smbus_mock.side_effect = IOError("boom")
-        self.device.disable_exceptions()
-        rc = self.device.write8(1, 0)
-        assert_equal(rc, -1)
-
     def test_generator(self):
 
-        for (test, name) in [
-            (self._test_one, 'test_one'),
-            (self._test_two, 'test_two')
+        for (method, smbus_method, args, rc) in [
+            ('write8', 'write_byte_data', (1, 0x70), None),
+            ('write16', 'write_word_data', (2, 0x12), None),
+            ('writeList', 'write_i2c_block_data', (3, [1, 2, 3, 4]), None),
+            ('readU8', 'read_byte_data', (4,), 0xab),
+            ('readS8', 'read_byte_data', (5,), -127),
+            ('readU16', 'read_word_data', (6,), 0x1234),
+            ('readS16', 'read_word_data', (7,), 0x4567),
+            ('readList', 'read_i2c_block_data', (8, 4), [1000, 1001, 1002, 1003])
         ]:
             for exc_mode in self.EXC_MODES:
-                test_func = partial(test, exc_mode)
-                test_func.description = '{}.{}.{}_{}'.format(
-                    __name__, type(self).__name__, name, self.EXC_MODE_NAME[exc_mode]
+                test_func = partial(self._test_device_access, method, smbus_method, exc_mode, args, rc)
+                test_func.description = '{}.{}.test_{}_{}'.format(
+                    __name__, type(self).__name__, method, self.EXC_MODE_NAME[exc_mode]
                 )
                 yield (test_func, )
 
-    @setup_i2c_device_exception('write8')
-    def _test_one(self):
-        print("Test one with flag", self.device._enable_exceptions)
-        self.device.write8(1, 0)
+    def _test_device_access(self, method, smbus_method, exc_mode, args, exp_rc):
 
-    @setup_i2c_device_exception
-    def _test_two(self):
-        print("Test two with flag", self.device._enable_exceptions)
+        cached_side_effect = getattr(self.device.bus, smbus_method).side_effect
+        
+        if exc_mode == self.EXC_MODE_NONE:
+            side_effect = None
+            exc_enable = False
+            getattr(self.device.bus, smbus_method).return_value = exp_rc
+        elif exc_mode == self.EXC_MODE_TRAP:
+            side_effect = IOError('mocked error')
+            exc_enable = False
+            exp_rc = I2CDevice.ERROR
+        elif exc_mode == self.EXC_MODE_RAISE:
+            side_effect = IOError('mocked error')
+            exc_enable = True
+        else:
+            raise Exception('Illegal exception test mode {}'.format(exc_mode))
+        
+        getattr(self.device.bus, smbus_method).side_effect = side_effect
+        self.device.enable_exceptions() if exc_enable else self.device.disable_exceptions()
+        
+        rc = None
+    
+        with assert_raises_regexp(I2CException, 'error from device') if exc_enable else dummy_cm():
+            rc = getattr(self.device, method)(*args)
+            assert_equal(rc, exp_rc)
+            
+        getattr(self.device.bus, smbus_method).side_effect = cached_side_effect
+        
+
+
